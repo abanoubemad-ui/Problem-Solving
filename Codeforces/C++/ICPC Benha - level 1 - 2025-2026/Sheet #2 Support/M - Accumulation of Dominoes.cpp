7aeb6330354961434e827942198a9778354f7ea5
Abanoub <abanoubemad499@gmail.com>
2026-06-22 23:20:13 +0300

    solve new problem

---
+++ Codeforces/C++/ICPC Benha - level 1 - 2025-2026/Sheet #2 Support/M - Accumulation of Dominoes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+
+    if (m == 1)
+    {
+        cout << n - 1 << endl;
+    }
+    else
+    {
+        cout << (m - 1) * n << endl;
+    }
+
+    return 0;
+}
