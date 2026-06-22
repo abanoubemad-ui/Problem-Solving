4d3d8d12374596520fd847747a26ad3023754d5e
Abanoub <abanoubemad499@gmail.com>
2026-06-22 23:34:43 +0300

    fix edge cases

--- Codeforces/C++/ICPC Benha - level 1 - 2025-2026/Sheet #2 conditions/Y - Cloning Toys.cpp
+++ Codeforces/C++/ICPC Benha - level 1 - 2025-2026/Sheet #2 conditions/Y - Cloning Toys.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main()
 {
-    int c, o;
+    long long c, o;
     cin >> c >> o;
 
-    if ((c > 0 && o == 0) || o == 0)
+    if ((c > 0 && o == 0) || (o == 0) || (o == 1 && c > 0))
     {
         cout << "NO" << endl;
     }
     else
     {
         int rc = c - (o - 1);
-        if (rc % 2 == 0)
+        if (rc % 2 == 0 && rc >= 0)
         {
             cout << "YES" << endl;
         }
